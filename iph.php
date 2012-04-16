7bd7a25c7d2296f581ea02961b03d9b2db8e3212
Léo Peltier <contact@leo-peltier.fr>
2012-04-16 16:56:25 +0200

    Added IPHPHP.

---
+++ iph.php
@@ -0,0 +1,77 @@
+<?php
+/** Image PlaceHolder for PHP.
+ * Usage : iph.php?<WIDTH>x<HEIGHT>
+ * Ex : iph.php?800x600
+ * */
+
+return main();
+
+/** Fetch the requested image size from $_SERVER['QUERY_STRING'].
+ * \returns ({x:int, y:int}) : queried image size.
+ * */
+function get_size_from_query() {
+	if(empty($_SERVER['QUERY_STRING']))
+		return null;
+
+	$query = explode('x', $_SERVER['QUERY_STRING']);
+	if(count($query) != 2)
+		return null;
+
+	list($x, $y) = array_map('intval', $query);
+	return (object) compact('x', 'y');
+}
+
+
+/** Draw an outer border.
+ * \param $img (GD2 image) : image.
+ * \param $size ({x:int, y:int}) : image size.
+ * \param $color (GD2 color) : color to use for the border.
+ * \param $thickness (int>0) : border thickness.
+ * */
+function imageborder(&$img, $size, $color = null, $thickness = 4) {
+	if(!$color)
+		$color = imagecolorallocate($img, 0, 0, 0);
+
+	$offset = floor($thickness/2);
+	$box = (object) array(
+		'ax' => $offset,
+		'ay' => $offset,
+		'bx' => $size->x - $offset - 1,
+		'by' => $size->y - $offset - 1
+	);
+
+	imagesetthickness($img, $thickness);
+	imagerectangle($img, $box->ax, $box->ay, $box->bx, $box->by, $color);
+}
+
+
+/// Get rid of output buffering.
+function ob_end_clean_all() {
+	$level = ob_get_level();
+	for($i=0; $i<$level; $i++) {
+		ob_end_clean();
+	}
+}
+
+
+/** Output a PNG from a GD2 image.
+ * \param $img (GD2 image) : image to show.
+ * */
+function print_png($img) {
+	ob_end_clean_all();
+	header('Content-Type: image/png');
+	imagepng($img);
+}
+
+
+/// Script entry point.
+function main() {
+	$size = get_size_from_query();
+
+	$img = imagecreatetruecolor($size->x, $size->y);
+	$filling = imagecolorallocate($img, 231, 241, 240);
+	imagefill($img, 0, 0, $filling);
+
+	print_png($img);
+}
+
